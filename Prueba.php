c790c65340632e6f8bcdd3af9edf6bcaa750bcd6
FabioGarciaSENA <fabiogarcia@misena.edu.co>
2023-09-27 19:03:55 -0500

    Second Commit

---
+++ Prueba.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$documento = $_POST['documento'];
+$edad = $_POST['edad'];
+
+echo 'Nombre:', $nombre ,' Apellido:', $apellido , ' documento:', $documento, ' edad:',  $edad;
+
+?>
